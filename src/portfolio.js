9a1d1b4e099b4e90f2ae03ca76fb4254735939cd
Rajat-tiwari48 <rajat6396344@gmail.com>
2025-07-18 13:22:20 +0530

    Updated Resume

--- src/portfolio.js
+++ src/portfolio.js
@@ -26,7 +26,7 @@ const greeting = {
     "Passionate about crafting cutting-edge web solutions to tackle real-world challenges with innovation and creativity. Committed to excellence, I transform ideas into impactful, user-centric designs."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1bqSCywnHkjwwUq00r6LXYeHAMlIaBKjM/view?usp=sharing", // Set to empty to hide the button
+    "https://drive.google.com/file/d/1AcbuDFPlXrTINN35FhmqlBylCdZE_mKz/view?usp=sharing", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
