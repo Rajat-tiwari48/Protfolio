a8a5575643fede33e880ac0c8588918fc7bab300
Rajat-tiwari48 <rajat6396344@gmail.com>
2025-10-05 10:17:40 +0530

    Update resume link in greeting section

--- src/portfolio.js
+++ src/portfolio.js
@@ -26,7 +26,7 @@ const greeting = {
     "Passionate about crafting cutting-edge web solutions to tackle real-world challenges with innovation and creativity. Committed to excellence, I transform ideas into impactful, user-centric designs."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/14OKtEM_X8m0hRGcmWPJU67pQHsKpNLpW/view?usp=sharing", // Set to empty to hide the button
+    "https://drive.google.com/file/d/1Dk3YMKQXiC8CtvUzwN5NZPc8bV5c5klw/view?usp=sharing", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
